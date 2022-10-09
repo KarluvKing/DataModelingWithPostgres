9392319f72fae7867fba5df42244f1b2bed0bc23
Rui Paiva Branco <email@ruipaivabranco.com>
2022-10-09 18:48:54 +0200

    Optimized for PEP8 guidelines

--- project/etl.py
+++ project/etl.py
@@ -5,9 +5,11 @@ import pandas as pd
 from sql_queries import *
 
 """
-This procedure processes a song file whose filepath has been provided as an arugment.
+This procedure processes a song file whose filepath has been
+provided as an arugment.
 It extracts the song information in order to store it into the songs table.
-Then it extracts the artist information in order to store it into the artists table.
+Then it extracts the artist information in order to store it into the
+artists table.
 
 INPUTS:
 * cur the cursor variable
@@ -41,10 +43,14 @@ def process_song_file(cur, filepath):
     cur.execute(artist_table_insert, artist_data)
 
 """
-This procedure processes a log file whose filepath has been provided as an arugment.
-It extracts time data records information in order to store it into the time table.
-It also extracts user data record information in order to store it into the user table.
-In the end extracts song and artists record information in order to store it into songplay table.
+This procedure processes a log file whose filepath has been
+provided as an arugment.
+It extracts time data records information in order to store it into
+the time table.
+It also extracts user data record information in order to store it into
+the user table.
+In the end extracts song and artists record information in order to store it
+into the songplay table.
 
 INPUTS:
 * cur the cursor variable
@@ -62,8 +68,15 @@ def process_log_file(cur, filepath):
     t = df['ts'] = pd.to_datetime(df['ts'], unit='ms')
 
     # insert time data records
-    time_data = t, t.dt.hour, t.dt.day, t.dt.weekofyear, t.dt.month, t.dt.year, t.dt.weekday
-    column_labels = ("Timestamp","Hour","Day","Week of the Year","Month","Year","Weekday")
+    time_data = t,
+    t.dt.hour,
+    t.dt.day,
+    t.dt.weekofyear,
+    t.dt.month,
+    t.dt.year,
+    t.dt.weekday
+    column_labels = (
+    "Timestamp","Hour","Day","Week of the Year","Month","Year","Weekday")
 
     time_df = {}
     for i,j in zip(time_data,column_labels):
@@ -93,13 +106,24 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        songplay_data = (
+        row.ts,
+        row.userId,
+        row.level,
+        songid,
+        artistid,
+        row.sessionId,
+        row.location,
+        row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 """
-This procedure processes all json files whose filepath has been provided as an arugment.
-Processes the data from json files whose filepath has been provided as an argument.
-Calls the functions process_song_file and process_log_file to extract the necessary data.
+This procedure processes all json files whose filepath has been provided
+as an arugment.
+Processes the data from json files whose filepath has been provided as
+an argument.
+Calls the functions process_song_file and process_log_file to extract the
+necessary data.
 
 INPUTS:
 * cur the cursor variable
@@ -127,14 +151,16 @@ def process_data(cur, conn, filepath, func):
         print('{}/{} files processed.'.format(i, num_files))
 
 """
-It creates the connection with the database and calls the functions to process the files.
+It creates the connection with the database and calls the functions to
+process the files.
 
 INPUTS:
 * No inputs
 """
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("
+    host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
